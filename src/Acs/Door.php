7233c346bc7471b9d785c109d546304887817758
Eden Leung <31346973+edenleung@users.noreply.github.com>
2021-04-22 13:32:32 +0800

    Update Door.php

--- src/Acs/Door.php
+++ src/Acs/Door.php
@@ -7,10 +7,22 @@ use Hik\Module;
 class Door extends Module
 {
     protected $urls = [
-        'events' => '/api/acs/v2/door/events'
+        'events' => '/api/acs/v2/door/events',
+        'status' => '/api/acs/v1/door/states',
+        'doControl' => '/api/acs/v1/door/doControl'
     ];
 
     public function events(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function status(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function doControl(array $data)
     {
         $this->data = $data;
     }
